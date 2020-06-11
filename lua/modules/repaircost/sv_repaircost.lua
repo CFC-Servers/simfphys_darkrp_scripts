7812cde157a478611644ae67d023bc739ddab6e2
PLally <11771225+PLally@users.noreply.github.com>
2020-06-11 00:21:03 -0400

    whitespaces

--- lua/modules/repaircost/sv_repaircost.lua
+++ lua/modules/repaircost/sv_repaircost.lua
@@ -7,35 +7,35 @@ local repairCost = 100
 
 swep.PrimaryAttack = function(self, ...)
     local owner = self.Owner
-	local money = owner:getDarkRPVar("money")
-	if money < repairCost then return end
-	
-	local trace = owner:GetEyeTrace()
-	local ent = trace.Entity
-	
-	local class = ent:GetClass()
-	
-	local isVehicle = class == "gmod_sent_vehicle_fphysics_base"
-	local isWheel = class == "gmod_sent_vehicle_fphysics_wheel"
-	
-	if isVehicle then
-		local func = ent.SetCurHealth
-		ent.SetCurHealth = function(...)
-			self.Owner:addMoney(-repairCost)
-			func(...)
-		end
-		primaryAttack(self, ...)
-		ent.SetCurHealth = func
-	elseif isWheel then
-		local func = ent.SetDamaged
-		ent.SetDamaged = function(...)
-			self.Owner:addMoney(-repairCost)
-			func(...)
-		end
-		primaryAttack(self, ...)
-		ent.SetDamaged = func
-	else
-		primaryAttack(self, ...)
-	end
+    local money = owner:getDarkRPVar("money")
+    if money < repairCost then return end
+    
+    local trace = owner:GetEyeTrace()
+    local ent = trace.Entity
+    
+    local class = ent:GetClass()
+    
+    local isVehicle = class == "gmod_sent_vehicle_fphysics_base"
+    local isWheel = class == "gmod_sent_vehicle_fphysics_wheel"
+    
+    if isVehicle then
+        local func = ent.SetCurHealth
+        ent.SetCurHealth = function(...)
+            self.Owner:addMoney(-repairCost)
+            func(...)
+        end
+        primaryAttack(self, ...)
+        ent.SetCurHealth = func
+    elseif isWheel then
+        local func = ent.SetDamaged
+        ent.SetDamaged = function(...)
+            self.Owner:addMoney(-repairCost)
+            func(...)
+        end
+        primaryAttack(self, ...)
+        ent.SetDamaged = func
+    else
+        primaryAttack(self, ...)
+    end
 end
 
