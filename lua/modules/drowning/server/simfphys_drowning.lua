81e94338aa9e15acba9d16d5aa29df792fc05e6e
Bleck <OverlordBleck@gmail.com>
2020-06-10 23:43:37 -0500

    Adding configurable variables; cleaning up code

--- lua/modules/drowning/server/simfphys_drowning.lua
+++ lua/modules/drowning/server/simfphys_drowning.lua
@@ -1,15 +1,24 @@
 local drowningCars = {}
-local drowningDelay = 1
-local drowningDamagePercent = 5
 local WATER_MAJORITY = 2
+
+local config = SimfphysDarkRPScripts.Config
 
 hook.Add( "Tick", "cfc_simfphys_drowning", function()
     for _, ent in pairs( ents.FindByClass( "gmod_sent_vehicle_fphysics_base" ) ) do
         if simfphys.IsCar( ent ) then
             if ent:WaterLevel() >= WATER_MAJORITY then
-                if CurTime() >= ( drowningCars[ent] or 0 ) + drowningDelay then
+                if CurTime() >= ( drowningCars[ent] or 0 ) + config.DrowningDelay then
+                    local tickDamage = ent:GetMaxHealth() * ( config.DrowningDamagePercent / 100 )
+                    local curHealth = ent:GetCurHealth()
+                    local owner = ent:CPPIGetOwner()
+
+                    ent:ApplyDamage( tickDamage )
+
+                    if tickDamage <= curHealth then
+                        owner:ChatPrint( config.DrownedVehicleMessage )
+                    end
+
                     drowningCars[ent] = CurTime()
-                    ent:ApplyDamage( ent:GetMaxHealth() * ( drowningDamagePercent / 100 ) )
                 end
             else
                 drowningCars[ent] = nil
