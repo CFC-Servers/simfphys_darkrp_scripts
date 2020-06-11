81a2dfb899d8b882da310025c98bf2ff9b78be07
Bleck <OverlordBleck@gmail.com>
2020-06-10 23:43:37 -0500

    Adding configurable variables; cleaning up code

---
+++ lua/modules/drowning/config/drowning_config.lua
@@ -0,0 +1,6 @@
+SimfphysDarkRPScripts.Config = SimfphysDarkRPScripts.Config or {}
+
+local config = SimfphysDarkRPScripts.Config
+config.DrowningDelay = 1 -- Seconds before each damage tick of a simfphys vehicle
+config.DrowningDamagePercent = 5 -- Damage *in percent* the simfphys vehicle takes from being in the water
+config.DrownedVehicleMessage = "" -- Message that is displayed when a persons simfphys vehicle is destroyed
