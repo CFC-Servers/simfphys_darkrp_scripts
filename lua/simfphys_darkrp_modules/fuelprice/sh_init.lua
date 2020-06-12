5ba339d74b0dbae174735cb520308eaf7cf63a4c
Brandon Sturgeon <sturgeonb4@gmail.com>
2020-06-12 04:27:12 -0400

    Fix addcsluafile stuff

--- lua/simfphys_darkrp_modules/fuelprice/sh_init.lua
+++ lua/simfphys_darkrp_modules/fuelprice/sh_init.lua
@@ -1,11 +1,14 @@
+if SERVER then
+    AddCSLuaFile()
+    AddCSLuaFile( "client/cl_fuel_pump_ui.lua" )
+end
+
 include( "sh_config.lua" )
 include( "sh_utils.lua" )
 
 if SERVER then
     include( "server/sv_main.lua" )
     include( "server/sv_net.lua" )
-
-    AddCSLuaFile( "client/cl_fuel_pump_ui.lua" )
 else
     include( "client/cl_fuel_pump_ui.lua" )
 end
