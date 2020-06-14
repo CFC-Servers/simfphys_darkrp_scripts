40500af1e209c9c43865e8d37c95df218b249d64
Brandon Sturgeon <sturgeonb4@gmail.com>
2020-06-13 23:35:27 -0400

    Strip fillerpistol too

--- lua/simfphys_darkrp_modules/fuelprice/sh_utils.lua
+++ lua/simfphys_darkrp_modules/fuelprice/sh_utils.lua
@@ -90,12 +90,16 @@ function FuelPrices:AddPumpExtensions( pump )
 
     if SERVER then
         function pump:ChargeCustomer()
+            local customer = pump.lastUser
             local finalPrice = pump:CalculateFuelPrice()
 
             if finalPrice == 0 then return end
 
+            -- Ensures we don't charge them more than they have
+            local customerMoney = customer:getDarkRPVar( "money" )
+            finalPrice = math.Clamp( finalPrice, 0, customerMoney )
+
             local formattedPrice = DarkRP.formatMoney( math.Round( finalPrice, 2 ) )
-            local customer = pump.lastUser
 
             local message = "You've been charged " .. formattedPrice .. " for fuel"
             DarkRP.notify( customer, 0, 5, message)
@@ -138,6 +142,7 @@ function FuelPrices:AddPumpExtensions( pump )
                         local message = "You can't afford any more fuel!"
                         DarkRP.notify( user, 1, 8, message )
 
+                        user:StripWeapon( "weapon_simfillerpistol" )
                         user.gas_InUse = false
                         pump:Disable()
                     end
