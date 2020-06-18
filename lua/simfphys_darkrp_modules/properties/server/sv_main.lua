274d48ddca4fde37ed58694a47916ca6ef65bc10
Bleck <OverlordBleck@gmail.com>
2020-06-17 23:11:51 -0500

    Ensuring that clients are unable to see the edit properties menu

--- lua/simfphys_darkrp_modules/properties/server/sv_main.lua
+++ lua/simfphys_darkrp_modules/properties/server/sv_main.lua
@@ -1,6 +1,8 @@
 hook.Add( "CanEditVariable", "CFC_Simfphys_PropertiesEdit", function( ent, ply, key, val, editor )
     if not ent.IsSimfphyscar then return end
     if ply:IsSuperAdmin() then return end
+
     ply:ChatPrint("You can not edit simfphys properties")
+    
     return false
 end )
