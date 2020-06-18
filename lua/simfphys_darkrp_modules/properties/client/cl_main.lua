e6be38d08c9eee7a3332bb6f17496fbef4398279
Bleck <OverlordBleck@gmail.com>
2020-06-17 23:15:32 -0500

    Removing redundant check

--- lua/simfphys_darkrp_modules/properties/client/cl_main.lua
+++ lua/simfphys_darkrp_modules/properties/client/cl_main.lua
@@ -1,5 +1,4 @@
 hook.Add( "OnEntityCreated", "CFC_OnSimfphysCreated", function( ent )
-    if not IsValid( ent ) then return end
     if ent:GetClass() ~= "gmod_sent_vehicle_fphysics_base" then return end
 
     -- One frame timer to prevent editing the variable while it has not itself
