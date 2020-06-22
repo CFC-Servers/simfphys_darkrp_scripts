074bae6ce3f17473e38cc5675e8d9cdb221bd9c9
Brandon Sturgeon <sturgeonb4@gmail.com>
2020-06-21 22:50:44 -0400

    Add taxes enabled

--- lua/simfphys_darkrp_modules/fuelprice/modules/taxes/sh_init.lua
+++ lua/simfphys_darkrp_modules/fuelprice/modules/taxes/sh_init.lua
@@ -4,4 +4,9 @@ include( "sh_utils.lua" )
 
 if SERVER then
     include( "server/sv_main.lua" )
+else
+    include( "client/cl_main.lua" )
 end
+
+FuelPrices.modules = {}
+FuelPrices.modules.taxes = true
